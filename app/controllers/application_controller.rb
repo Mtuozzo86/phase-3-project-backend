2a750149fd24f5d6688f23b9c831c4ac65d0b03c
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-12-03 12:22:37 -0500

    start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
+  set :default_content_type, "application/json"
+
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Good luck with your project!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" }.to_json
   end
 
+  get "/test" do
+    "test backend".to_json
+  end
+
+  get "/another" do
+    "another backend".to_json
+  end
 end
