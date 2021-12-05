27cf417485c111136b76d4d8ee47682017f149bf
Mtuozzo86 <mtuozzo86@gmail.com>
2021-12-05 08:43:23 -0500

    update

--- db/migrate/20211206175001_remove_barbershop_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBarbershopTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :barbershops
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ end
 
 Employee.create(name: "Jesse")
 Employee.create(name: "Joseph")
-Review.create(body: "Good job!", customer_id: customer.id, employee_id: employee.id)
+
 
 
 puts "✅ Done seeding!"
