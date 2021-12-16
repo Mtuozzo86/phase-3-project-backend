195bb58d51b8f5e7db6bbd928e8c7ee9cd577e2c
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-12-16 15:17:17 -0500

    more fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/reviews" do
-
     user = Customer.create(name: params[:userName])
     user.to_json
     review = Review.create(body: params[:body], employee_id: params[:barber], customer_id: user.id)
     review.to_json
-    
+  end
+
+  get "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.to_json
   end
 
   delete "/reviews/:id" do
